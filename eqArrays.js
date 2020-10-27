a7f9fac7885848f57d89e210ce94beda95af7ef5
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-27 23:17:05 +0000

    Comparing elements of two arrays and returning true or false

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
