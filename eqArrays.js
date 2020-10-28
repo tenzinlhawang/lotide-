7933eb6d535fee56e50e9fcdbdb1242837ca439f
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-28 17:41:43 +0000

    Updated my file

--- eqArrays.js
+++ eqArrays.js
@@ -8,13 +8,12 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function (arr1, arr2) {
   for(let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    } else {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+  return true;
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
