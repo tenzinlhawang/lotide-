36de5c35c08558649e67ef80e37b423e6312d52f
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-28 23:39:11 +0000

    Wrote a function to return the number of occurences for each letter in an array of array

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const eqArrays = function (arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArrayEqual(letterPositions("hello").e, [1]);
