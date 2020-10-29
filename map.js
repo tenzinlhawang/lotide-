a6af26fb361887f61f875f9be0f99d87dfa17214
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-29 23:55:01 +0000

    Implementing the map function

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
