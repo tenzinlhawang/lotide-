cceb288972c3efe8926129daba8238a3d0f63282
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-29 00:16:20 +0000

    Function to scan the object and return the first key which contains the value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, val) {
+  const objKeys = Object.keys(obj);
+  // console.log(objKeys);
+  for (const key of objKeys) {
+    if (obj[key] === val){
+      return key;
+    }
+}
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
