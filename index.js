905ef2f44b8ae85576ba2b2189d555c2a143aa12
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-02 21:59:58 +0000

    Updated index file using ES6

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const reverse = require('./reverse');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  reverse,
+  takeUntil,
+  without
+};
