67a75608e5e4f8668fbcebef06f752b33062462b
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-02 19:31:14 +0000

    Recfactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
