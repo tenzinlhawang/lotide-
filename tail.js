f1c14ccdd3766d4a030675c79d57935a01b782bc
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-02 19:34:15 +0000

    Recfactored tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   return arr.filter((elem, index) => index !== 0);
