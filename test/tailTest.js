8c4312b35c5fa57bfa1845317aaf690ec21b553d
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-02 20:39:33 +0000

    Refactored using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#tail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {  
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {  
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+
+});
+
