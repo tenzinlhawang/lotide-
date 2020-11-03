d51c8e0e1106c1e587986b3ad6d8d447a4120b92
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-03 01:19:46 +0000

    Refactored without.js

--- without.js
+++ without.js
@@ -1,20 +1,6 @@
-const assertEqual = function(arr1, arr2) {
-  if (arr1 === arr2) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else if (arr1 !== arr2) {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function (arr1, arr2) {
-  for(let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+const eqArrays = require('./eqArrays');
 
 const without = function (source, itemsToRemove) {
   const result = [];
@@ -23,5 +9,7 @@ const without = function (source, itemsToRemove) {
     return result;
 
 }
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+// without([1, 2, 3], [1]) // => [2, 3]
+// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+module.exports = without;
