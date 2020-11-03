4f74d42428c6276f46f98503fa6ef1c341d5f251
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-03 01:19:11 +0000

    Refactored takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,22 +1,6 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for(let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const takeUntil = function(array, callback) {
   let result = [];
@@ -29,6 +13,8 @@ const takeUntil = function(array, callback) {
   }
 }
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+module.exports = takeUntil;
