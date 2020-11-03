0e20e809cd345937334ecf7e4921d68abe1deed3
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-03 01:16:49 +0000

    Refactored findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
+/*
 const findKeyByValue = function (obj, val) {
   const objKeys = Object.keys(obj);
   // console.log(objKeys);
@@ -15,11 +9,21 @@ const findKeyByValue = function (obj, val) {
     }
 }
 };
+*/
+const findKeyByValue = function (obj, val) {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+}
+
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
