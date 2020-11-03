5866062681c32231b7effaa2de8a80a879579d3b
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-03 01:07:49 +0000

    Refactored letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,10 @@
-const eqArrays = function (arr1, arr2) {
-  for(let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] !== ' ') {
       if (results[sentence[i]]) {
@@ -30,4 +17,6 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-assertArrayEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
